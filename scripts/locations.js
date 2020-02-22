cf4105d0da415b625d02bc01e01438e9a53362ed
Keartroth <mecarrolljr@gmail.com>
2020-02-21 20:06:21 -0600

    added location tips and click events for each location visited

---
+++ scripts/locations.js
@@ -0,0 +1,53 @@
+const initializeTravelTipsEvents = () => {
+    // CAN'T TOUCH THIS - START
+    const allCloseButtons = document.querySelectorAll(".button--close")
+
+    for (const btn of allCloseButtons) {
+        btn.addEventListener(
+            "click",
+            theEvent => {
+                const dialogElement = theEvent.target.parentNode
+                dialogElement.close()
+            }
+        )
+    }
+    // CAN'T TOUCH THIS - END
+
+
+    // You will be writing code below this line
+    document.querySelector("#bonaireImg").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#tips--bonaire")
+            theDialog.showModal()
+        }
+    )
+
+    document.querySelector("#thailandImg").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#tips--thailand")
+            theDialog.showModal()
+        }
+    )
+
+    document.querySelector("#reefImg").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#tips--reef")
+            theDialog.showModal()
+        }
+    )
+
+    document.querySelector("#indianImg").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#tips--indian")
+            theDialog.showModal()
+        }
+    )
+}
+
+
+initializeTravelTipsEvents();
+// export default initializeTravelTipsEvents;
--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,20 @@
  *  reference and then invoke it
  */
 import initializeDetailButtonEvents from './dialogs.js'
+// import initializeTravelTipsEvents from './locations.js'
+
 
 initializeDetailButtonEvents();
+// initializeTravelTipsEvents();
+
+
+
+
+
+// if (window.location.hostname === "http://localhost:5000/locations/locations") {
+//     // import initializeTravelTipsEvents from './locations.js'
+//     initializeTravelTipsEvents();
+// } else if (window.location.hostname === "http://localhost:5000/") {
+//     // import initializeDetailButtonEvents from './dialogs.js'
+//     initializeDetailButtonEvents();
+// }
