bf15cefb67c417dec8ef6b8efbe5c9c0249acef1
Keartroth <mecarrolljr@gmail.com>
2020-02-26 15:40:53 -0600

    added javascripts object array to hold all fish objects

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,20 @@
  *  reference and then invoke it
  */
 import initializeDetailButtonEvents from './dialogs.js'
+import { useFish } from './FishDataProvider.js'
 // import initializeTravelTipsEvents from './locations.js'
 
 
 initializeDetailButtonEvents();
+useFish();
 // initializeTravelTipsEvents();
 
 
+const arrayOfTheFishObjects = useFish();
 
+for (const currentFishObject of arrayOfTheFishObjects) {
+    console.log(currentFishObject);
+}
 
 
 // if (window.location.hostname === "http://localhost:5000/locations/locations") {
